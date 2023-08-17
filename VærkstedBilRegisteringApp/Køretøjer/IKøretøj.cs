9a521cf8f7e360302ea30534bb7d6ea163de9abf
ThomasSmidt <smidt592@gmail.com>
2023-08-17 09:06:33 +0200

    Removed and sorted usings

--- VærkstedBilRegisteringApp/Køretøjer/IKøretøj.cs
+++ VærkstedBilRegisteringApp/Køretøjer/IKøretøj.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-
-namespace VærkstedBilRegisteringApp.Køretøjer
+﻿namespace VærkstedBilRegisteringApp.Køretøjer
 {
     internal class IKøretøj
     {
